4f79b00314e051a5ed84333eedd189b9265908c0
Cole Kendall <mrcoole7890@outlook.com>
2024-03-13 21:05:45 -0400

    add helper function

--- Classes/Display.js
+++ Classes/Display.js
@@ -57,27 +57,19 @@ class Display{
     }
 
     setCanvasWidth() {
-        if (this.enviorment == null) {
-            console.warn("Attempting to set canvas width when there is no enviorment set. Setting width to 0.")
-           this.canvasElement.setAttribute("width", 0)
-        }            
-        else if (this.enviorment.getMaze() == null){
-           console.warn("Attempting to set canvas width when there is no maze set. Setting width to 0.")
-           this.canvasElement.setAttribute("width", 0)
-        }    
+        if (this.enviorment == null)
+            this.warnAndRevertAttribute("Attempting to set canvas width when there is no enviorment set. Setting width to 0.", "width")
+        else if (this.enviorment.getMaze() == null)
+           this.warnAndRevertAttribute("Attempting to set canvas width when there is no maze set. Setting width to 0.", "width")
         else
            this.canvasElement.setAttribute("width", this.enviorment.getMaze().getWidth() * this.tileSize)
     }
     
     setCanvasHeight() {
-        if (this.enviorment == null) {
-            console.warn("Attempting to set canvas height when there is no enviorment set. Setting height to 0.")
-           this.canvasElement.setAttribute("height", 0)
-        }            
-        else if (this.enviorment.getMaze() == null){
-           console.warn("Attempting to set canvas height when there is no maze set. Setting height to 0.")
-           this.canvasElement.setAttribute("height", 0)
-        }   
+        if (this.enviorment == null)
+            this.warnAndRevertAttribute("Attempting to set canvas height when there is no enviorment set. Setting height to 0.", "height")
+        else if (this.enviorment.getMaze() == null)
+           this.warnAndRevertAttribute("Attempting to set canvas height when there is no maze set. Setting height to 0.", "height")
         else
            this.canvasElement.setAttribute("height", this.enviorment.getMaze().getWidth() * this.tileSize)
     }
@@ -89,9 +81,20 @@ class Display{
                 this.ctx.fillRect(this.tileSize * j, this.tileSize * i, this.tileSize, this.tileSize)                  
             }
         }
-        this.ctx.fillStyle = this.tileMapping.get("Player")
-        this.ctx.fillRect(this.tileSize * this.enviorment.player.getCords()[1], this.tileSize * this.enviorment.player.getCords()[0], this.tileSize, this.tileSize)
-        this.ctx.fillStyle = this.tileMapping.get("Goal")
-        this.ctx.fillRect(this.tileSize * this.enviorment.goal.getCords()[1], this.tileSize * this.enviorment.goal.getCords()[0], this.tileSize, this.tileSize)  
-    }   
+        this.drawPoint(this.enviorment.player, "Player")
+        this.drawPoint(this.enviorment.goal, "Goal")
+    }
+
+    drawPoint(pointObj, tileName) {
+        if (pointObj.constructor.name != "Point") {
+            console.warn("attempting to draw an unexpected object: " + pointObj.constructor.name)
+            return
+        }
+        this.ctx.fillStyle = this.tileMapping.get(tileName)
+        this.ctx.fillRect(this.tileSize * pointObj.getCords()[1], this.tileSize * pointObj.getCords()[0], this.tileSize, this.tileSize)
+    }
+    warnAndRevertAttribute(warning, attributeName) {
+           console.warn(warning)
+           this.canvasElement.setAttribute(attributeName, 0)
+    }  
 }
