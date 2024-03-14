111e8f688327e9384d5ba78e5691085b29e761bb
Cole Kendall <mrcoole7890@outlook.com>
2024-03-13 20:26:11 -0400

    update handling

--- Classes/EventHandling.js
+++ Classes/EventHandling.js
@@ -1,14 +1,21 @@
 class EventHandling {
 
     constructor() {
-        document.addEventListener("keyup", this.handelKeyUpEvent)
-        document.addEventListener("keydown", this.handelKeyDownEvent)
         this.activeKeys = new Map()
+        document.addEventListener("keyup", (event) => {
+            this.handelKeyUpEvent(event)
+        })
+        document.addEventListener("keydown", (event) =>{
+            this.handelKeyDownEvent(event)
+        })
+        document.addEventListener("blur", (event) =>{
+            this.handelKeyUpEvent(event)
+        })
     }
 
     handelKeyUpEvent(event) {
         if (this.activeKeys.has(event))
-            this.activeKeys.set(event.key, false) 
+            this.activeKeys.set(event.key, false)
         else
             console.warn("attempting to set unmonitored key (" + event.key + ") to inactive.")   
     } 
