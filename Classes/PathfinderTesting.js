9da2e777bf22211867f4caf24610990d206f1338
Cole Kendall <mrcoole7890@outlook.com>
2024-03-20 00:54:18 -0400

    add testing for valid and invalid enviroment parameters

--- Classes/PathfinderTesting.js
+++ Classes/PathfinderTesting.js
@@ -4,6 +4,7 @@ class PathfinderTesting {
 
         this.testInvalidTypeParameters()
         this.testValidTypeParameters()
+        this.testEnviormentParameters()
     }
 
     testInvalidTypeParameters() {
@@ -45,12 +46,8 @@ class PathfinderTesting {
         enviorment.setPlayerLocation([0,0])
         enviorment.setGoalLocation([2,2])
         var testData = new Array()
-        var invalidTypeValues = [0, 1, 2]
         
         var AllTestsPass = true 
-
-        for (var i = 0; i < invalidTypeValues.length; i++)
-            testData.push(["Pathfinder", new Pathfinder(invalidTypeValues[i], enviorment).constructor.name])
 
         for (let i = 0; i < testData.length; i++)
             AllTestsPass = AllTestsPass && this.test.isEquals(testData[i][0], testData[i][1])
@@ -59,6 +56,46 @@ class PathfinderTesting {
             console.log("Pathfinder valid type parameter handeling works as expected!")
         else
             console.error("Pathfinder valid type parameter handeling is not working as expected...")
+
+    }
+
+    testEnviormentParameters() {
+        var maze = new Maze([
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+        ])
+
+        var testData = new Array()
+        var enviorment = new Enviorment()
+
+        testData.push([null, new Pathfinder(0, enviorment).enviorment])
+
+        enviorment.setMaze(maze)
+
+        testData.push([null, new Pathfinder(0, enviorment).enviorment])
+
+        enviorment.setPlayerLocation([0,0])
+
+        testData.push([null, new Pathfinder(0, enviorment).enviorment])
+
+        enviorment.setGoalLocation([2,2])
+
+        testData.push(["Enviorment", new Pathfinder(0, enviorment).enviorment.constructor.name])
+
+        enviorment.setMaze(null)
+
+        testData.push([null, new Pathfinder(0, enviorment).enviorment])
+        
+        var AllTestsPass = true 
+
+        for (let i = 0; i < testData.length; i++)
+            AllTestsPass = AllTestsPass && this.test.isEqualsWithMessage(testData[i][0], testData[i][1], i)
+
+        if (AllTestsPass)
+            console.log("Pathfinder enviorment parameter handeling works as expected!")
+        else
+            console.error("Pathfinder enviorment parameter handeling is not working as expected...")
 
     }
 }
