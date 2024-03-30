c08ed12a70462eb2e18ca7d0719663ca8f7c2920
Cole Kendall <mrcoole7890@outlook.com>
2024-03-29 20:28:01 -0400

    add specs to Maze

--- spec/Maze.spec.js
+++ spec/Maze.spec.js
@@ -1,7 +1,7 @@
 var Mazes = require("../Classes/Maze.js")
 
 describe('Maze Class', () => {
-    it('should return a string version of its self', () => {
+    it('should return a string version of its self when valid parameters are passed', () => {
         var twoByTwoMaze = new Mazes.Maze([[0,1],[0,0]])
         var threeByThreeMaze = new Mazes.Maze([[0,1,0],[0,0,0],[1,0,0]])
         var expectedTwoByTwoMazeToString = " 0 1 \n 0 0 \n "
@@ -9,5 +9,10 @@ describe('Maze Class', () => {
 
         expect(twoByTwoMaze.getMazeAsString()).toBe(expectedTwoByTwoMazeToString)
         expect(threeByThreeMaze.getMazeAsString()).toBe(expectedThreeByThreeMazeToString)
+    });
+    it('should return null when invalid parameters are passed', () => {
+        expect(new Mazes.Maze([[0,1],[]]).getMazeAsString()).toBe(null)
+        expect(new Mazes.Maze(3).getMazeAsString()).toBe(null)
+        expect(new Mazes.Maze([[0,1,0],[0,0],[1,0,0]]).getMazeAsString()).toBe(null)
     });
 });
