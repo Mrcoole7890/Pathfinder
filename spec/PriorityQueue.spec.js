32d63fd202d3b102d365fd785271bd7ad5dd5dbe
Cole Kendall <mrcoole7890@outlook.com>
2024-04-01 17:46:41 -0400

    implemented test cases

--- spec/PriorityQueue.spec.js
+++ spec/PriorityQueue.spec.js
@@ -11,12 +11,27 @@ describe('PriorityQueue class', () => {
         expect(pqOldLength).toBe(pqOldLength + 1)
     });
     it('when push is called, takes item and value params and adds them to the heap', () => {
-        fail("unimplemented test case")
+        var pq = new PriorityQueues.PriorityQueue()
+        var pushValue = "string value"
+        pq.push(pushValue)
+        expect(pq.list.includes(pushValue)).toBe(true)
     });
     it('when pop is called, the node with the lowest value in the heap is retruned', () => {
-        fail("unimplemented test case")
+        var pq = new PriorityQueues.PriorityQueue()
+        var expectedValue = 0x1
+        pq.push(expectedValue, 0)
+        pq.push("random value", 5)
+        pq.push(21, 7)
+        expect(pq.pop()).toBe(expectedValue)
     });
     it('when pop is called and the length of the list is not 0, decrement the length value of list', () => {
-        fail("unimplemented test case")
+        var pq = new PriorityQueues.PriorityQueue()
+        var expectedValue = 0x1
+        pq.push(expectedValue, "this")
+        pq.push("random value", 0x0)
+        pq.push(21, true)
+        var qExpectedLength = 3
+        pq.pop()
+        expect(pq.length).toBe(qExpectedLength - 1)
     });
 });
--- spec/Queue.spec.js
+++ spec/Queue.spec.js
@@ -8,18 +8,34 @@ describe('Queue class', () => {
     it('when push is called, add one to the length of the list', () => {
         var q = new Queues.Queue()
         var qOldLength = q.length
-        expect(qOldLength).toBe(qOldLength + 1)
+        q.push(5)
+        expect(q.length).toBe(qOldLength + 1)
     });
     it('when push is called, takes item params and adds it to the queue', () => {
-        fail("unimplemented test case")
+        var q = new Queues.Queue()
+        var pushValue = "string value"
+        q.push(pushValue)
+        expect(q.list.includes(pushValue)).toBe(true)
     });
     it('when push is called, the value parameter is thrown out', () => {
         fail("unimplemented test case")
     });
     it('when pop is called, the front node in the list is retruned', () => {
-        fail("unimplemented test case")
+        var q = new Queues.Queue()
+        var expectedValue = 0x1
+        q.push(expectedValue, null)
+        q.push("random value", 64)
+        q.push(21, false)
+        expect(q.pop()).toBe(expectedValue)
     });
     it('when pop is called and the length of the list is not 0, decrement the length value of list', () => {
-        fail("unimplemented test case")
+        var q = new Queues.Queue()
+        var expectedValue = 0x1
+        q.push(expectedValue, "this")
+        q.push("random value", 0x0)
+        q.push(21, true)
+        var qExpectedLength = 3
+        q.pop()
+        expect(q.length).toBe(qExpectedLength - 1)
     });
 });
