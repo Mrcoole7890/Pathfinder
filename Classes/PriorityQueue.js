2a6a6aec9d0b38a3c7bfd63dcb7d1c00764bc458
Cole Kendall <mrcoole7890@outlook.com>
2024-04-01 16:51:04 -0400

    add comments and parameters

--- Classes/PriorityQueue.js
+++ Classes/PriorityQueue.js
@@ -1,14 +1,18 @@
 class PriorityQueue {
 
+    // when intialized the length should be zero
     constructor() {
         this.list = []
         this.length = 0
     }
 
-    push() {
+    // add one to the length of the list
+    // takes item and value params and adds them to the heap
+    push(item, value) {
 
     }
 
+    // returns the node with the lowest value in the heap
     pop() {
         
     }
--- Classes/Queue.js
+++ Classes/Queue.js
@@ -1,14 +1,19 @@
 class Queue {
 
+    // when intialized sets length should be zero
     constructor() {
         this.list = []
         this.length = 0
     }
 
-    push() {
+    // add one to the length of the list
+    // takes item params and adds it to the queue
+    // the value parameter is thrown out
+    push(item, value) {
 
     }
 
+    // returns the front node in the list
     pop() {
         
     }
