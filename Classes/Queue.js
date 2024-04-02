56bf3356cbb6705fc068216830052237211be744
Cole Kendall <mrcoole7890@outlook.com>
2024-04-02 16:43:40 -0400

    implement queue class

--- Classes/Queue.js
+++ Classes/Queue.js
@@ -10,14 +10,20 @@ class Queue {
     // takes item params and adds it to the queue
     // the value parameter is thrown out
     push(item, value) {
-
+        this.length++
+        this.list = [item].concat(this.list)
     }
 
     // returns the front node in the list
     // when the queue is empty pop should return null
     // decrement the length value of list if the length of the list is not 0
     pop() {
-        
+        if (this.length == 0)
+            return
+        else {
+            this.length--
+            return this.list.pop()
+        }
     }
 
 }
